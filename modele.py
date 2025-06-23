46787487d54acf99377848c89056f563c81253da
PaulMarchiset <paul.marchiset@edu.univ-fcomte.fr>
2025-06-23 13:59:45 +0200

    Implement database connection and student retrieval functionality

---
+++ modele.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    database='allocimac'
+)
+
+mycursor = mydb.cursor(dictionary=True)
+
+def getAllStudents():
+    mycursor.execute("SELECT * FROM ETUDIANT")
+    students = mycursor.fetchall()
+    return students
+
+def getStudentById(id):
+    mycursor.execute("SELECT e.nom, g.nom, f.nom FROM ETUDIANT e JOIN GENRE g ON e.id_genre = g.id JOIN FILM f ON e.id_film=f.id WHERE id = %s", (id,))
+    student = mycursor.fetchone()
+    return student
+
--- script.py
+++ script.py
@@ -1,3 +1,18 @@
+import mysql.connector
+
+from modele import getAllStudents, getStudentById
+
 from flask import Flask, render_template, request
 
-app = Flask("__name__")
+app = Flask(__name__)
+
+def index():
+    return render_template("index.html")
+
+@app.route("/", methods=["GET"])
+def home():
+    return index()
+
+@app.route("/student", methods=["GET"])
+def student():
+    return render_template("student.html", students=getAllStudents())
