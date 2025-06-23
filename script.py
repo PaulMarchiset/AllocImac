0b44a22f291240d9585541d0907c07c456e473d2
Lilian Peuron <lilianpeuron@gmail.com>
2025-06-23 11:59:45 +0200

    Add Tailwind, home.html, logo component

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
 from flask import Flask, render_template, request
 
-app = Flask("__name__")
+app = Flask(__name__)
+
+def index():
+    return render_template("index.html")
+
+@app.route("/")
+def home():
+    return render_template("pages/home.html")
